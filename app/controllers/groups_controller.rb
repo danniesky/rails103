99f76e65b48aa8d955d3f0181cea525f08289561
danniesky <decmoon1201@gmail.com>
2017-01-01 20:35:34 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,15 +18,15 @@ class GroupsController < ApplicationController
   @group = Group.new
   end
 
-  def create
+def create
     @group = Group.new(group_params)
     @group.user = current_user
 
-    if@group.save
+    if @group.save
       redirect_to groups_path
-  else
-    render :new
-  end
+    else
+      render :new
+    end
   end
 
  def update
